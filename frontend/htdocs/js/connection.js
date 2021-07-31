5973c907ded406f44e78f2a6258f725582e1c2cd
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-07-31 18:33:00 +0900

    サーバへの接続処理を実装

--- frontend/htdocs/js/connection.js
+++ frontend/htdocs/js/connection.js
@@ -1,3 +1,17 @@
+var sock = null;
+window.onload = () => {
+    sock = new WebSocket("ws://127.0.0.1:3000");
+    sock.addEventListener("message", (e) => {
+        console.log(e);
+    });
+
+    sock.addEventListener("error", (e) => {
+        alert("Socket Error!");
+        console.log(e);
+        sock = null;
+    });
+}
+
 const sendMessage = (message) => {
     buildDom(message, true);
 }
